74e41535aaf94339db071d74c230ccf2130d31ff
lucky4all <lucasromerocoscia@gmail.com>
2025-06-16 10:41:31 -0300

    styles: added responsive design

--- components/cabezera.js
+++ components/cabezera.js
@@ -9,12 +9,18 @@ const roboto = Roboto({
 
 export default function Cabezera() {
     return (
-        <header className="flex flex-row bg-gray-700 h-16 shadow-2xs shadow-black items-center justify-between px-8">
-            <p className="text-purple-500 text-3xl font-bold border-r-4 border-purple-500 pr-4 hover:text-purple-600 cursor-pointer transition-colors">⚡ Lucky</p>
-            <Link href="/" id="enlace" className={roboto.className}>Inicio</Link>
-            <Link href="/contacto" id="enlace" className={roboto.className}>Contacto</Link>
-            <Link href="proyectos" id="enlace" className={roboto.className}>Proyectos</Link>
-            <button className="bg-yellow-400 text-black p-2 rounded-xl font-bold cursor-pointer border-black border-2 hover:bg-amber-400 transition-colors">Contratame</button>
+        <header className="flex flex-row bg-gray-700 h-16 shadow-2xs shadow-black items-center justify-between px-4 sm:px-8 lg:px-16">
+            <p className="text-purple-500 text-2xl sm:text-3xl font-bold border-r-4 border-purple-500 pr-2 sm:pr-4 hover:text-purple-600 cursor-pointer transition-colors">
+                ⚡ Lucky
+            </p>
+            <nav className="flex gap-2 sm:gap-4 lg:gap-8 items-center">
+                <Link href="/" id="enlace" className={roboto.className + " text-base sm:text-lg"}>Inicio</Link>
+                <Link href="/contacto" id="enlace" className={roboto.className + " text-base sm:text-lg"}>Contacto</Link>
+                <Link href="proyectos" id="enlace" className={roboto.className + " text-base sm:text-lg"}>Proyectos</Link>
+                <button className="bg-yellow-400 text-black px-2 py-1 sm:px-4 sm:py-2 rounded-xl font-bold cursor-pointer border-black border-2 hover:bg-amber-400 transition-colors text-sm sm:text-base">
+                    Contratame
+                </button>
+            </nav>
         </header>
     )
 }
